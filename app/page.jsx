ea81bd68ccd250bd0d8688f8c77a99522aa26e14
Elmar099 <elmar6999@gmail.com>
2024-05-10 13:44:49 -0700

    initialized

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+            Web Personal Trainer
+            <br className="max-md:hidden" />
+            <span className="orange_gradient text-center">Discover & Share Workouts </span>
+        </h1>
+        <p className="desc text-center">Join our vibrant fitness community and discover a world of motivation, 
+            support, and collaboration as you create and share workout routines 
+            to inspire others on their fitness journey
+        </p>
+        {/* FEED */}
+    </section>
+  )
+}
+
+export default Home
